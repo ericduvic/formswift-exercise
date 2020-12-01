4840054e90daab91589dc16522f2c1353e3650f0
Eric Duvic <ericduvic@gmail.com>
2020-12-01 11:27:13 -0700

    Fix linebreak in python testing script

--- tests/tests.py
+++ tests/tests.py
@@ -8,5 +8,6 @@ ENDPOINT_URL = os.environ.get("ENDPOINT_URL")
 class HelloWorldTest(TestCase):
     def test_endpoint(self):
         url = f"{ENDPOINT_URL}"
-        response = requests.get(url)self.assertEqual(res.status_code, 200)
+        response = requests.get(url)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(res.text, "Hello World!")
